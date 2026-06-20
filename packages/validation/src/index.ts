2be827a8ed0fd2d7385a5139d84045a781516b72
Ansh Deep <ad0811@srmist.edu.in>
2026-06-20 22:17:38 +0530

    add createTask validation

--- packages/validation/src/index.ts
+++ packages/validation/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth/signUp.js";
 export * from "./auth/login.js";
 export * from "./user/createWorker.js";
+export * from "./task/createTask.js";
---
+++ packages/validation/src/task/createTask.ts
@@ -0,0 +1,22 @@
+import {z} from 'zod';
+
+export const createTaskSchema = z.object({
+    id: z.uuid().optional(),
+    title: z.string().trim().min(4,{error: "Title is required and must be at least 4 characters long"}).max(200,{error: "Title must be at most 200 characters long"}),
+    description: z.string().trim().min(10, {error: "Description is required and must be at least 10 characters long"}).max(5000,{error: "Description must be at most 5000 characters long"}),
+    assignedUserId: z.uuid().optional(),
+    checkListItems: z.array(z.object({
+        id: z.uuid().optional(),
+        content: z.string().trim().min(1,{error: "Checklist item content is required and must be at least 1 character long"}).max(500,{error: "Checklist item content must be at most 500 characters long"}),
+    })).max(100, {error: "A task can have at most 100 checklist items"}).optional()
+})
+
+export type CreateTaskInput = z.infer<typeof createTaskSchema>;
+
+export const paramsSchema = z.object({
+    itemId: z.string().uuid(), // if IDs are UUIDs
+});
+
+export const bodySchema = z.object({
+    isCompleted: z.boolean(),
+});
